b6930d1344f8b8e63a93ea62c7b2fd138995e39f
PhuongHuu <huu.phuong195@gmail.com>
2017-08-20 22:02:25 +0700

    Base project. Laravel 5.4 | VueJS When create new project, change to this commit to get empty project

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+</div>
+<!-- END wrapper -->
+<!-- jQuery  -->
+<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('assets/js/metisMenu.min.js') }}"></script>
+<script src="{{ asset('assets/js/waves.js') }}"></script>
+<script src="{{ asset('assets/js/jquery.slimscroll.js') }}"></script>
+<!-- App js -->
+<script src="{{ asset('assets/js/jquery.core.js') }}"></script>
+<script src="{{ asset('assets/js/jquery.app.js') }}"></script>
+</body>
+</html>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,29 @@
+<!-- ========== Left Sidebar Start ========== -->
+<div class="left side-menu">
+  <div class="slimscroll-menu" id="remove-scroll">
+    <!--- Sidemenu -->
+    <div id="sidebar-menu">
+      <!-- Left Menu Start -->
+      <ul class="metisMenu nav" id="side-menu">
+        <li class="menu-title">More</li>
+        <li>
+          <a href="javascript: void(0);" aria-expanded="true"><i class="fi-share"></i> <span>Multi Level</span> <span class="menu-arrow"></span></a>
+          <ul class="nav-second-level nav" aria-expanded="true">
+            <li><a href="javascript: void(0);">Level 1.1</a></li>
+            <li>
+              <a href="javascript: void(0);" aria-expanded="true">Level 1.2 <span class="menu-arrow"></span></a>
+              <ul class="nav-third-level nav" aria-expanded="true">
+                <li><a href="javascript: void(0);">Level 2.1</a></li>
+                <li><a href="javascript: void(0);">Level 2.2</a></li>
+              </ul>
+            </li>
+          </ul>
+        </li><!-- /.End parent menu -->
+      </ul><!-- /.End menu -->
+    </div>
+    <!-- Sidebar -->
+    <div class="clearfix"></div>
+  </div>
+  <!-- Sidebar -left -->
+</div>
+<!-- Left Sidebar End -->
