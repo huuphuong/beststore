1bbed9458c516b3dde5b842a13b66e89207118a9
HuuPhuong <phuongcao.ask@gmail.com>
2017-09-13 09:00:30 +0700

    Update view

--- resources/assets/js/components/users/UserList.vue
+++ resources/assets/js/components/users/UserList.vue
@@ -1,106 +1,110 @@
 <template>
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h3 class="panel-title">Danh sách user</h3>
-		</div>
-		<div class="panel-body">
-			<table class="table table-hover">
-				<thead>
-					<tr>
-						<th>Index</th>
-						<th>UserID</th>
-						<th>Username</th>
-						<th>E-mail</th>
-						<th>Phone</th>
-						<th>Gender</th>
-						<th>Updated At</th>
-						<th>Role</th>
-						<th width="15%">Action</th>
-					</tr>
-
-					<tr>
-						<th>#</th>
-						<th>
-							<input type="text" v-model="query.id" class="form-control">
-						</th>
-
-						<th>
-							<input type="text" v-model="query.name" class="form-control">
-						</th>
-
-						<th>
-							<input type="text" v-model="query.email" class="form-control">
-						</th>
-
-						<th>
-							<input type="text" v-model="query.phone" class="form-control">
-						</th>
-
-						<th>
-							<input type="text" v-model="query.gender" class="form-control">
-						</th>
-
-						<th>
-							<input type="text" v-model="query.created_at" class="form-control">
-						</th>
-
-						<th>
-							<input type="text" v-model="query.role" class="form-control">
-						</th>
-
-						<th>
-							<div class="btn-group">
-								<button type="button" class="btn btn-primary" @click="getListUser(1, query)">Filter</button>
-								<!-- <button type="button" class="btn btn-default" @click="clearPage">Cancel</button> -->
-								<a href="/users" class="btn btn-default">Cancel</a>
-							</div>
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr v-for="user, key in users">
-						<td>{{ indexNumber(++key) }}</td>
-						<td>U{{ user.id }}</td>
-						<td>{{ user.name }}</td>
-						<td>{{ user.email }}</td>
-						<td>{{ user.phone }}</td>
-						<td>{{ user.gender }}</td>
-						<td>{{ user.created_at }}</td>
-						<td>{{ user.role_id }}</td>
-						<td>
-
-						</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-		<div class="panel-footer">
-			<div class="row">
-				<div class="col-sm-12">
-					<div class="col-sm-3">
-					<ul class="pagination">
-						<li>
-							<strong>Total: {{ total }}</strong>
-						</li>
-					</ul><!-- /.pagination -->
-
-					</div><!-- /.col-sm-3 -->
-					<div class="col-sm-6">
-						<paginate
-						:page-count="last_page"
-						:click-handler="getListUser"
-						:page-range="3"
-						:margin-pages="2"
-						:prev-text="'Trước'"
-						:next-text="'Sau'"
-						:container-class="'pagination'"
-						:page-class="'page-item'">
-					</paginate>
-				</div>
-			</div><!-- /.col-sm-12 -->
-		</div>
-	</div>
-</div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h3 class="panel-title">Danh sách user</h3>
+    </div>
+    <div class="panel-body">
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th>Index</th>
+            <th>UserID</th>
+            <th>Username</th>
+            <th>E-mail</th>
+            <th>Phone</th>
+            <th>Gender</th>
+            <th>Updated At</th>
+            <th>Role</th>
+            <th width="15%">Action</th>
+          </tr>
+          <tr>
+            <th>#</th>
+            <th>
+              <input type="text" v-model="query.id" class="form-control">
+            </th>
+            <th>
+              <input type="text" v-model="query.name" class="form-control">
+            </th>
+            <th>
+              <input type="text" v-model="query.email" class="form-control">
+            </th>
+            <th>
+              <input type="text" v-model="query.phone" class="form-control">
+            </th>
+            <th>
+              <input type="text" v-model="query.gender" class="form-control">
+            </th>
+            <th>
+              <input type="text" v-model="query.created_at" class="form-control">
+            </th>
+            <th>
+              <input type="text" v-model="query.role" class="form-control">
+            </th>
+            <th>
+              <div class="btn-group">
+                <button type="button" class="btn btn-primary" @click="getListUser(1, query)">Filter</button>
+                <!-- <button type="button" class="btn btn-default" @click="clearPage">Cancel</button> -->
+                <a href="/users" class="btn btn-default">Cancel</a>
+              </div>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user, key in users">
+            <td>{{ indexNumber(++key) }}</td>
+            <td>U{{ user.id }}</td>
+            <td>{{ user.name }}</td>
+            <td>{{ user.email }}</td>
+            <td>{{ user.phone }}</td>
+            <td>{{ user.gender }}</td>
+            <td>{{ user.created_at }}</td>
+            <td>{{ user.role_id }}</td>
+            <td>
+              <div class="dropdown">
+                <button class="btn btn-link dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                <span class="glyphicon glyphicon-option-horizontal"></span>
+                </button>
+                <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu1">
+                  <li><a href="#">
+                    <span class="glyphicon glyphicon-pencil"></span> Edit User
+                    </a>
+                  </li>
+                  <li><a href="#"><span class="glyphicon glyphicon-trash"></span> View User Detail</a></li>
+                </ul>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="panel-footer">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="col-sm-3">
+            <ul class="pagination">
+              <li>
+                <strong>Total: {{ total }}</strong>
+              </li>
+            </ul>
+            <!-- /.pagination -->
+          </div>
+          <!-- /.col-sm-3 -->
+          <div class="col-sm-6">
+            <paginate
+              :page-count="last_page"
+              :click-handler="getListUser"
+              :page-range="3"
+              :margin-pages="2"
+              :prev-text="'Trước'"
+              :next-text="'Sau'"
+              :container-class="'pagination'"
+              :page-class="'page-item'">
+            </paginate>
+          </div>
+        </div>
+        <!-- /.col-sm-12 -->
+      </div>
+    </div>
+  </div>
 </template>
-
 <script src="./UserList.js"></script>
